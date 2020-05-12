849147a7ade6d70d43cdcd47e42b33efb0132dd8
jpcole76 <64864521+jpcole76@users.noreply.github.com>
2020-05-11 21:38:36 -0500

    refactor: Corrected spelling of a lot in line 26 of the jc.py file
    -Updated the spelling from alot to a lot in line 26 of the jc.py file

--- jc.py
+++ jc.py
@@ -23,7 +23,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     #Added for git Project
-    print('This should be alot of fun!')
+    print('This should be a lot of fun!')
 
     city=input("\nSelect one of the following cities:\nChicago \nNew York City \nWashington \nType your selection here: ").lower()
     while city not in cities:
